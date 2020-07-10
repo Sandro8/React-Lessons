9cb5478a068609e54fd61a53e85e68413168f8ca
Sandro8 <bigjig@yandex.ru>
2020-07-10 20:45:32 +0300

    Разбивка новых файлов на компоненты

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <main className="main">
+            <img src="https://miro.medium.com/max/1000/1*MnP5DPXdh9RJnwtwrVXl6A.jpeg" alt="" />
+        </main>
+    );
+}
+export default Content;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <aside className="nav">
+            Profile<br />
+            News<br />
+            Messages<br />
+            Music<br />
+            Settings<br />
+        </aside>
+    );
+}
+export default Sidebar;
