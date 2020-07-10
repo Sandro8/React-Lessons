d709c4489eccec4d3c1618eadbabfa0359c23ec4
Sandro8 <bigjig@yandex.ru>
2020-07-10 15:10:18 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Primer from './Primer.js';
+import './App.css';
+import Proba from './proba.js';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Primer />
+      <Proba />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Primer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Primer = () => {
+    return <h1>Пример заголовка через компонент</h1>
+}
+
+export default Primer;
