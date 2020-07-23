38f2d98794d280d2a97a645e7184ef714e77c58f
Sandro8 <bigjig@yandex.ru>
2020-07-23 21:22:19 +0300

    Новая страница Messages. Navlink. Урок 22

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import './Messages.module.css'
+import i from './Messages.module.css'
 import Post from '../Posts/post';
+import { NavLink } from 'react-router-dom';
 
 const Messages = (props) => {
     return (
-        <div>
+        <div className={i.Messages}>
             <div>
-                <textarea></textarea>
+                <div>
+                    <NavLink to="/Messages/1" activeClassName={i.active}>Василий</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/Messages/2" activeClassName={i.active}>Андрей</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/Messages/3" activeClassName={i.active}>Сергей</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/Messages/4" activeClassName={i.active}>Григорий</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/Messages/5" activeClassName={i.active}>Дмитрий</NavLink>
+                </div>
             </div>
-            <button>Add Post</button>
-            <Post message="Здравствуй, это пропс" />
-            <Post message="Это вот тоже через пропс" />
-            <Post message="Hello" />
-            <Post message="How are you?" />
-            <Post message="Some text" />
-        </div>
+            <div className={i.middle}></div>
+            <div>
+                <Post message="Здравствуй, это пропс" />
+                <Post message="Это вот тоже через пропс" />
+                <Post message="Hello" />
+                <Post message="How are you?" />
+                <Post message="Some text" />
+            </div>
+        </div >
     )
 
 }
